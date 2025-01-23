c33d4429ac0275b9b3a8f7d6a69465040d488b35
Nikson Dantas <niksondantas@gmail.com>
2025-01-23 16:41:32 -0300

    feat: finaliza lógica, conteúdo e estilo do quizz

--- js/script.js
+++ js/script.js
@@ -96,6 +96,137 @@ const questionNumber = question.querySelector("#question-number")
 questionText.textContent = questions[i].question;
 questionNumber.textContent = i + 1
 
+//Insere as alternativas
+questions[i].answers.forEach(function(answer, i){
+
+  //Cria o template do botão do quizz
+  const answerTemplate = document.querySelector(".answer-template").cloneNode(true)
+  
+  const letterBtn = answerTemplate.querySelector(".btn-letter")
+  const answerText = answerTemplate.querySelector(".question-answer")
+
+  letterBtn.textContent = letters[i]
+  answerText.textContent = answer['answer']
+  
+  answerTemplate.setAttribute("correct-answer", answer["correct"])
+
+  //Remover hide e template class
+  answerTemplate.classList.remove("hide")
+  answerTemplate.classList.remove("answer-template")
+
+  //Inserir a alternativa na tela
+  answersBox.appendChild(answerTemplate)
+
+  //Inserir um evento de click no botão
+  answerTemplate.addEventListener("click", function(){
+    checkAnswer(this)
+  })
+
+})
+
+//Incrementar o número da questão
+actualQuestion++
+
+
 }
-//Chamando a função
+
+//Verificando resposta do usuário
+function checkAnswer(btn){
+
+  //Seleciona todos os botões
+  const buttons = answersBox.querySelectorAll("button")
+
+  //Verifica se a resposta está correta e adiciona classes nos botões
+  buttons.forEach(function(button){
+
+    if(button.getAttribute("correct-answer") === "true"){
+
+      button.classList.add("correct-answer")
+
+      //Checa se o usuário acertou a pergunta
+      if(btn === button){
+
+        //Se acertou incrementa os pontos
+        points++
+
+      }
+    }else{
+
+      button.classList.add("wrong-answer")
+
+    }
+
+  })
+
+  //Exibir próxima pergunta
+  nextQuestion()
+
+}
+
+//Exibe a próxima pergunta do Quizz
+function nextQuestion(){
+
+  //Time para o usuário ver as respostas
+  setTimeout(function(){
+  
+    //Verifica se ainda há perguntas
+    if(actualQuestion >= questions.length){
+
+      //Mensagem sucesso
+      showSuccessMesage()
+      return
+
+    }
+
+    createQuestion(actualQuestion)
+
+  }, 1500)
+
+}
+
+//Exibe a tela final
+function showSuccessMesage(){
+
+  hideOrShowQuizz()
+
+  //Trocar informações da tela de sucesso
+
+  //Calcular score
+  const score = ((points / questions.length) * 100).toFixed(2)
+
+  const displayScore = document.querySelector("#display-score span")
+  displayScore.textContent = score.toString()
+
+  //Altera o número de perguntas corretas
+  const correctAnswer = document.querySelector("#correct-answers")
+  correctAnswer.textContent = points
+
+  //Alterar o toatal de perguntas
+  const totalQuestions = document.querySelector("#questions-qty")
+  totalQuestions.textContent = questions.length
+
+}
+
+//Mostra ou esconde o score
+function hideOrShowQuizz(){
+
+  quizzContainer.classList.toggle("hide")
+  scoreContainer.classList.toggle("hide")
+
+}
+
+//Reiniciar Quizz
+const restartBtn = document.querySelector("#restart")
+
+restartBtn.addEventListener("click", function(){
+
+  //Zerar o jogo
+  actualQuestion = 0
+  points = 0
+  hideOrShowQuizz()
+  init()
+
+})
+
+//Chamando a inicialização do Quizz
 init()
